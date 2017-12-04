8263e1ad18fb2cd6646c4aa1d03bce7655236679
Charles Moulliard <cmoulliard@redhat.com>
2017-12-04 17:41:55 +0100

    Clean imports, remove non used methods. Add ImageStreams

--- src/main/java/org/workspace7/cloud/fabric8/IstioEnricher.java
+++ src/main/java/org/workspace7/cloud/fabric8/IstioEnricher.java
@@ -3,26 +3,17 @@ package org.workspace7.cloud.fabric8;
 import io.fabric8.kubernetes.api.builder.TypedVisitor;
 import io.fabric8.kubernetes.api.model.*;
 import io.fabric8.kubernetes.api.model.extensions.DeploymentBuilder;
-import io.fabric8.kubernetes.api.model.extensions.DeploymentSpec;
-import io.fabric8.kubernetes.api.model.extensions.DeploymentSpecBuilder;
-import io.fabric8.maven.core.config.ResourceConfig;
 import io.fabric8.maven.core.handler.DeploymentHandler;
 import io.fabric8.maven.core.handler.HandlerHub;
 import io.fabric8.maven.core.util.Configs;
 import io.fabric8.maven.core.util.MavenUtil;
+import io.fabric8.maven.docker.config.BuildImageConfiguration;
 import io.fabric8.maven.docker.config.ImageConfiguration;
 import io.fabric8.maven.enricher.api.BaseEnricher;
 import io.fabric8.maven.enricher.api.EnricherContext;
-import io.fabric8.openshift.api.model.DeploymentConfigBuilder;
-import io.fabric8.openshift.api.model.DeploymentConfigSpecBuilder;
-import io.fabric8.openshift.api.model.TemplateBuilder;
-import io.vertx.core.json.JsonArray;
-import io.vertx.core.json.JsonObject;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 /**
  * This enricher takes care of adding <a href="https://isito.io">Istio</a> related enrichments to the Kubernetes Deployment
@@ -45,8 +36,11 @@ public class IstioEnricher extends BaseEnricher {
         proxyName {{
             d = "istio-proxy";
         }},
-        proxyImage {{
-            d = "proxy_debug:latest";
+        proxyDockerImageName {{
+            d = "docker.io/istio/proxy_debug:0.2.12";
+        }},
+        proxyImageStreamName {{
+            d = "proxy_debug";
         }},
         proxyArgs {{
             d = "proxy,sidecar,-v,2,--configPath,/etc/istio/proxy,--binaryPath,/usr/local/bin/envoy,--serviceCluster,app-cluster-name," +
@@ -56,14 +50,20 @@ public class IstioEnricher extends BaseEnricher {
         initName {{
             d = "istio-init";
         }},
-        initImage {{
-            d = "proxy_init:latest";
+        initDockerImageName {{
+            d = "docker.io/istio/proxy_init:0.2.12";
+        }},
+        initImageStreamName {{
+            d = "proxy_init";
         }},
         coreDumpName {{
             d = "enable-core-dump";
         }},
-        coreDumpImage {{
-            d = "alpine:latest";
+        coreDumpDockerImageName {{
+            d = "alpine";
+        }},
+        coreDumpImageStreamName {{
+            d = "alpine";
         }},
         imagePullPolicy {{
             d = "IfNotPresent";
@@ -271,7 +271,7 @@ public class IstioEnricher extends BaseEnricher {
                           .withName(getConfig(Config.proxyName))
                           .withResources(new ResourceRequirements())
                           .withTerminationMessagePath("/dev/termination-log")
-                          .withImage(getConfig(Config.proxyImage))
+                          .withImage(getConfig(Config.proxyImageStreamName))
                           .withImagePullPolicy(getConfig(Config.imagePullPolicy))
                           .withArgs(sidecarArgs)
                           .withEnv(proxyEnvVars())
@@ -288,12 +288,14 @@ public class IstioEnricher extends BaseEnricher {
                   }
               }
           });
+
+        getContext().getImages().addAll(istioImages());
     }
 
     protected Container istioInitContainer() {
         /*
           .put("name", "istio-init")
-          .put("image", getConfig(Config.initImage))
+          .put("image", getConfig(Config.initImageStreamName))
           .put("imagePullPolicy", "IfNotPresent")
           .put("resources", new JsonObject())
           .put("terminationMessagePath", "/dev/termination-log")
@@ -313,7 +315,7 @@ public class IstioEnricher extends BaseEnricher {
 
         return new ContainerBuilder()
             .withName(getConfig(Config.initName))
-            .withImage(getConfig(Config.initImage))
+            .withImage(getConfig(Config.initImageStreamName))
             .withImagePullPolicy("IfNotPresent")
             .withTerminationMessagePath("/dev/termination-log")
             .withTerminationMessagePolicy("File")
@@ -347,7 +349,7 @@ public class IstioEnricher extends BaseEnricher {
          */
         return new ContainerBuilder()
             .withName(getConfig(Config.coreDumpName))
-            .withImage(getConfig(Config.coreDumpImage))
+            .withImage(getConfig(Config.coreDumpImageStreamName))
             .withImagePullPolicy("IfNotPresent")
             .withCommand("/bin/sh")
             .withArgs("-c","sysctl -w kernel.core_pattern=/etc/istio/proxy/core.%e.%p.%t && ulimit -c unlimited")
@@ -357,121 +359,6 @@ public class IstioEnricher extends BaseEnricher {
                 .withPrivileged(true)
                 .build())
             .build();
-    }
-
-    //TODO - need to check if istio proxy side car is already there
-    //TODO - adding init-containers to template spec
-    //TODO - find out the other container  liveliness/readiness probes
-    public void addMissingNotWorkingResources(KubernetesListBuilder builder) {
-
-        String[] proxyArgs = getConfig(Config.proxyArgs).split(",");
-        List<String> sidecarArgs = new ArrayList<>();
-        for (int i = 0; i < proxyArgs.length; i++) {
-            sidecarArgs.add(proxyArgs[i]);
-        }
-
-        builder.accept(new TypedVisitor<DeploymentConfigBuilder>() {
-            public void visit(DeploymentConfigBuilder deploymentConfigBuilder) {
-                if ("yes".equalsIgnoreCase(getConfig(Config.enabled))) {
-                    log.info("Adding Istio proxy");
-                    String initContainerJson = buildInitContainers();
-                    sidecarArgs.add("--passthrough");
-                    sidecarArgs.add("8080");
-
-                    /* Proxy Def
-                    * args:
-                    *     - proxy
-                    *     - sidecar
-                    *     - -v
-                    *     - "2"
-                    *     - --configPath
-                    *     - /etc/istio/proxy
-                    *     - --binaryPath
-                    *     - /usr/local/bin/envoy
-                    *     - --serviceCluster
-                    *     - helloworld
-                    *     - --drainDuration
-                    *     - 45s
-                    *     - --parentShutdownDuration
-                    *     - 1m0s
-                    *     - --discoveryAddress
-                    *     - istio-pilot.istio-system:8080
-                    *     - --discoveryRefreshDelay
-                    *     - 1s
-                    *     - --zipkinAddress
-                    *     - zipkin.istio-system:9411
-                    *     - --connectTimeout
-                    *     - 10s
-                    *     - --statsdUdpAddress
-                    *     - istio-mixer.istio-system:9125
-                    *     - --proxyAdminPort
-                    *     - "15000"
-                    * env:
-                    * - name: POD_NAME
-                    * valueFrom:
-                    * fieldRef:
-                    * fieldPath: metadata.name
-                    *     - name: POD_NAMESPACE
-                    * valueFrom:
-                    * fieldRef:
-                    * fieldPath: metadata.namespace
-                    *     - name: INSTANCE_IP
-                    * valueFrom:
-                    * fieldRef:
-                    * fieldPath: status.podIP
-                    * image: docker.io/istio/proxy_debug:0.2.12
-                    * imagePullPolicy: IfNotPresent
-                    * name: istio-proxy
-                    * resources: {}
-                    * securityContext:
-                    *   privileged: true
-                    *   readOnlyRootFilesystem: false
-                    *   runAsUser: 1337
-                    * volumeMounts:
-                    * - mountPath: /etc/istio/proxy
-                    *   name: istio-envoy
-                    * - mountPath: /etc/certs/
-                    *   name: istio-certs
-                    *   readOnly: true
-                    */
-
-                    deploymentConfigBuilder
-                          // MetaData
-                          .editOrNewMetadata()
-                            .addToAnnotations("sidecar.istio.io/status", "injected-version-releng@0d29a2c0d15f-0.2.12-998e0e00d375688bcb2af042fc81a60ce5264009")
-                            .addToAnnotations("sidecar.istio.io/status",initContainerJson)
-                          .endMetadata()
-                          // Spec part of the DeploymentConfig
-                          .editOrNewSpec()
-                            .editOrNewTemplate()
-                              .editOrNewMetadata()
-                                .addToAnnotations("sidecar.istio.io/status", "injected-version-releng@0d29a2c0d15f-0.2.12-998e0e00d375688bcb2af042fc81a60ce5264009")
-                                .addToAnnotations("sidecar.istio.io/status",initContainerJson)
-                              .endMetadata()
-                              .editOrNewSpec()
-                                .addNewContainer()
-                                  // See Proxy def
-                                  .withName(getConfig(Config.proxyName))
-                                  .withResources(new ResourceRequirements())
-                                  .withTerminationMessagePath("/dev/termination-log")
-                                  .withImage(getConfig(Config.proxyImage))
-                                  .withImagePullPolicy(getConfig(Config.imagePullPolicy))
-                                  .withArgs(sidecarArgs)
-                                  .withEnv(proxyEnvVars())
-                                  .withSecurityContext(new SecurityContextBuilder()
-                                      .withRunAsUser(1337l)
-                                      .withPrivileged(true)
-                                      .withReadOnlyRootFilesystem(false)
-                                      .build())
-                                  .withVolumeMounts(istioVolumeMounts())
-                                .endContainer()
-                                .withVolumes(istioVolumes())
-                              .endSpec()
-                            .endTemplate()
-                        .endSpec();
-                }
-            }
-        });
     }
 
     /**
@@ -553,94 +440,33 @@ public class IstioEnricher extends BaseEnricher {
         return envVars;
     }
 
-    /**
-     * Builds the isito init containers that needs to be added to Istio Deployments via annotation
-     * &quot;pod.beta.kubernetes.io/init-containers&quot;
-     *
-     * @return Json string that will set as value of the annotation
-     */
-    protected String buildInitContainers() {
+    private List<ImageConfiguration> istioImages() {
+        List<ImageConfiguration> images = new ArrayList<>();
+        ImageConfiguration.Builder builder = new ImageConfiguration.Builder();
+        builder
+            .name(getConfig(Config.proxyImageStreamName))
+            .buildConfig(new BuildImageConfiguration.Builder()
+               .from(getConfig(Config.proxyDockerImageName))
+            .build());
+        images.add(builder.build());
 
-        JsonArray initContainers = new JsonArray();
+        builder = new ImageConfiguration.Builder();
+        builder
+            .name(getConfig(Config.initImageStreamName))
+            .buildConfig(new BuildImageConfiguration.Builder()
+                .from(getConfig(Config.initDockerImageName))
+                .build());
+        images.add(builder.build());
 
-        /* Istio Proxy Init
-         *   args:
-         *     - '-p'
-         *     - '15001'
-         *     - '-u'
-         *     - '1337'
-         *   image: 'docker.io/istio/proxy_init:0.2.12'
-         *   imagePullPolicy: IfNotPresent
-         *   name: istio-init
-         *   resources: {}
-         *   securityContext:
-         *     capabilities:
-         *       add:
-         *         - NET_ADMIN
-         *     privileged: true
-         *   terminationMessagePath: /dev/termination-log
-         *   terminationMessagePolicy: File
-         */
-        JsonObject initContainer1 = new JsonObject()
-            .put("name", "istio-init")
-            .put("image", getConfig(Config.initImage))
-            .put("imagePullPolicy", "IfNotPresent")
-            .put("resources", new JsonObject())
-            .put("terminationMessagePath", "/dev/termination-log")
-            .put("terminationMessagePolicy", "File")
-            .put("args", new JsonArray()
-                .add("-p")
-                .add("15001")
-                .add("-u")
-                .add("1337"))
-            .put("securityContext",
-                new JsonObject()
-                    .put("capabilities",
-                        new JsonObject()
-                            .put("add", new JsonArray().add("NET_ADMIN")))
-                    .put("privileged",true));
+        builder = new ImageConfiguration.Builder();
+        builder
+            .name(getConfig(Config.coreDumpImageStreamName))
+            .buildConfig(new BuildImageConfiguration.Builder()
+                .from(getConfig(Config.coreDumpDockerImageName))
+                .build());
+        images.add(builder.build());
 
-        initContainers.add(initContainer1);
-
-        /* Enable Core Dump
-         *  args:
-         *   - '-c'
-         *   - >-
-         *     sysctl -w kernel.core_pattern=/etc/istio/proxy/core.%e.%p.%t &&
-         *     ulimit -c unlimited
-         * command:
-         *   - /bin/sh
-         * image: alpine
-         * imagePullPolicy: IfNotPresent
-         * name: enable-core-dump
-         * resources: {}
-         * securityContext:
-         *   privileged: true
-         * terminationMessagePath: /dev/termination-log
-         * terminationMessagePolicy: File
-         */
-        JsonObject initContainer2 = new JsonObject()
-            .put("name", "enable-core-dump")
-            .put("image", getConfig(Config.coreDumpImage))
-            .put("imagePullPolicy", "IfNotPresent")
-            .put("command", new JsonArray().add("/bin/sh"))
-            .put("resources", new JsonObject())
-            .put("terminationMessagePath", "/dev/termination-log")
-            .put("terminationMessagePolicy", "File")
-            .put("args", new JsonArray()
-                .add("-c")
-                .add("sysctl -w kernel.core_pattern=/etc/istio/proxy/core.%e.%p.%t \u0026\u0026 ulimit -c unlimited"))
-            .put("securityContext",
-                new JsonObject()
-                    .put("privileged", true))
-            .put("terminationMessagePath", "/dev/termination-log")
-            .put("terminationMessagePolicy", "File");
-
-        initContainers.add(initContainer2);
-
-        String json = initContainers.encode();
-        log.debug("Adding Init Containers {}", json);
-        return json;
+        return images;
     }
 
 }
