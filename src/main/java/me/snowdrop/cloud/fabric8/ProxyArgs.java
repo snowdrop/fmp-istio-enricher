6ee70f699d7e9a15df50efa6437e65db92e4a1c8
Charles Moulliard <cmoulliard@redhat.com>
2017-12-07 17:40:01 +0100

    Add new file

---
+++ src/main/java/me/snowdrop/cloud/fabric8/ProxyArgs.java
@@ -0,0 +1,54 @@
+package me.snowdrop.cloud.fabric8;
+
+public enum ProxyArgs {
+    RELEASE_0_2_12("0.2.12","proxy,sidecar" +
+                            "-v,2," +
+                            "--configPath,/etc/istio/proxy," +
+                            "--binaryPath,/usr/local/bin/envoy," +
+                            "--serviceCluster,APP_CLUSTER_NAME," +
+                            "--drainDuration,45s," +
+                            "--parentShutdownDuration,1m0s," +
+                            "--discoveryAddress,istio-pilot.ISTIO_NAMESPACE:8080," +
+                            "--discoveryRefreshDelay," +
+                            "--discoveryRefreshDelay,1s," +
+                            "--zipkinAddress,zipkin.ISTIO_NAMESPACE:9411," +
+                            "--connectTimeout,10s," +
+                            "--statsdUdpAddress,istio-mixer.ISTIO_NAMESPACE:9125," +
+                            "--proxyAdminPort,15000"),
+
+    RELEASE_0_3_0("0.3.12","proxy,sidecar" +
+                           "-v,2," +
+                           "--configPath,/etc/istio/proxy," +
+                           "--binaryPath,/usr/local/bin/envoy," +
+                           "--serviceCluster,APP_CLUSTER_NAME," +
+                           "--drainDuration,45s," +
+                           "--parentShutdownDuration,1m0s," +
+                           "--discoveryAddress,istio-pilot.ISTIO_NAMESPACE:8080," +
+                           "--discoveryRefreshDelay," +
+                           "--discoveryRefreshDelay,1s," +
+                           "--zipkinAddress,zipkin.ISTIO_NAMESPACE:9411," +
+                           "--connectTimeout,10s," +
+                           "--statsdUdpAddress,istio-mixer.ISTIO_NAMESPACE:9125," +
+                           "--proxyAdminPort,15000," +
+                           "controlPlaneAuthPolicy,NONE");
+
+    private final String version;
+    private final String args;
+
+    ProxyArgs(String version, String args) {
+        this.version = version;
+        this.args = args;
+    }
+    public String getArgs() {
+        return args;
+    }
+
+    public static ProxyArgs findByRelease(String release){
+        for(ProxyArgs v : values()){
+            if( v.version.equals(release)){
+                return v;
+            }
+        }
+        return null;
+    }
+}
