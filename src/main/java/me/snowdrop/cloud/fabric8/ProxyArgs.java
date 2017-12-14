682da3370fffb41e1aa44faee5e23afd73b36ac7
Charles Moulliard <cmoulliard@redhat.com>
2017-12-14 11:17:13 +0100

    Use KEYWORD to find/replace urls of the servers

--- src/main/java/me/snowdrop/cloud/fabric8/ProxyArgs.java
+++ src/main/java/me/snowdrop/cloud/fabric8/ProxyArgs.java
@@ -2,35 +2,35 @@ package me.snowdrop.cloud.fabric8;
 
 public enum ProxyArgs {
     RELEASE_0_2_12("0.2.12",
-                            "proxy,sidecar," +
-                            "-v,2," +
-                            "--configPath,/etc/istio/proxy," +
-                            "--binaryPath,/usr/local/bin/envoy," +
-                            "--serviceCluster,APP_CLUSTER_NAME," +
-                            "--drainDuration,45s," +
-                            "--parentShutdownDuration,1m0s," +
-                            "--discoveryAddress,ISTIO_PILOT_ADDRESS," +
-                            "--discoveryRefreshDelay,1s," +
-                            "--zipkinAddress,ZIPKIN_ADDRESS," +
-                            "--connectTimeout,10s," +
-                            "--statsdUdpAddress,MIXER_ADDRESS," +
-                            "--proxyAdminPort,15000"),
+                   "proxy,sidecar," +
+                   "-v,2," +
+                   "--configPath,/etc/istio/proxy," +
+                   "--binaryPath,/usr/local/bin/envoy," +
+                   "--serviceCluster,APP_CLUSTER_NAME," +
+                   "--drainDuration,45s," +
+                   "--parentShutdownDuration,1m0s," +
+                   "--discoveryAddress,ISTIO_PILOT_ADDRESS," +
+                   "--discoveryRefreshDelay,1s," +
+                   "--zipkinAddress,ZIPKIN_ADDRESS," +
+                   "--connectTimeout,10s," +
+                   "--statsdUdpAddress,MIXER_ADDRESS," +
+                   "--proxyAdminPort,15000"),
 
     RELEASE_0_3_0("0.3.0",
-                           "proxy,sidecar," +
-                           "-v,2," +
-                           "--configPath,/etc/istio/proxy," +
-                           "--binaryPath,/usr/local/bin/envoy," +
-                           "--serviceCluster,APP_CLUSTER_NAME," +
-                           "--drainDuration,45s," +
-                           "--parentShutdownDuration,1m0s," +
-                           "--discoveryAddress,istio-pilot.ISTIO_NAMESPACE:8080," +
-                           "--discoveryRefreshDelay,1s," +
-                           "--zipkinAddress,zipkin.ISTIO_NAMESPACE:9411," +
-                           "--connectTimeout,10s," +
-                           "--statsdUdpAddress,istio-mixer.ISTIO_NAMESPACE:9125," +
-                           "--proxyAdminPort,15000," +
-                           "controlPlaneAuthPolicy,NONE");
+                  "proxy,sidecar," +
+                  "-v,2," +
+                  "--configPath,/etc/istio/proxy," +
+                  "--binaryPath,/usr/local/bin/envoy," +
+                  "--serviceCluster,APP_CLUSTER_NAME," +
+                  "--drainDuration,45s," +
+                  "--parentShutdownDuration,1m0s," +
+                  "--discoveryAddress,ISTIO_PILOT_ADDRESS," +
+                  "--discoveryRefreshDelay,1s," +
+                  "--zipkinAddress,ZIPKIN_ADDRESS," +
+                  "--connectTimeout,10s," +
+                  "--statsdUdpAddress,MIXER_ADDRESS," +
+                  "--proxyAdminPort,15000," +
+                  "controlPlaneAuthPolicy,NONE");
 
     private final String version;
     private final String args;
